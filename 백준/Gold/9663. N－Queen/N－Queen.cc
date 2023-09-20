12a2bfc068b6d50ad105855818d7e997b0303c05
Ju Jae Myeong <74813599+haru1367@users.noreply.github.com>
2023-09-20 22:37:24 +0900

    [Gold IV] Title: N-Queen, Time: 4308 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+int N; // N*N보드판
+int answer = 0;
+int col[15];
+
+void queen(int x) {
+	if (N == x) {  // 모든 행에 배치가 가능한 경우 경우의수 +1
+		answer++;
+	}
+	else {
+		for (int i = 0; i < N; i++) {
+			col[x] = i;
+			bool check = true; // 퀸의 배치가 가능한지 체크
+			for (int j = 0; j < x; j++) {
+				if (col[x] == col[j] || abs(col[x] - col[j]) == x - j) {//같은 열에 있거나 대각선상에 있을때는 제외
+					check = false;
+					break;
+				}
+			}
+			if (check == true) {
+				queen(x + 1); // 배치가 가능할 시 다음 행으로 넘어감
+			}
+		}
+	}
+}
+
+int main() {
+	cin.tie(0);
+	cout.tie(0);
+	ios_base::sync_with_stdio(false);
+	cin >> N;
+	queen(0);  // 첫번째 행 부터 배치 시작
+	cout << answer << '\n';
+}
