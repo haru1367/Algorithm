4cc41714f44ff1e733b2a6ca2ceed781a262c0bf
Ju Jae Myeong <74813599+haru1367@users.noreply.github.com>
2024-04-12 19:14:21 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+
+using namespace std;
+
+int dh[4] = {0,1,-1,0};
+int dw[4] = {1,0,0,-1};
+
+int solution(vector<vector<string>> board, int h, int w) {
+    int answer = 0;
+    int n = board.size();
+    for (int i = 0; i < 4; i++) {
+        int h_check = h + dh[i];
+        int w_check = w + dw[i];
+        if (h_check < 0 || h_check >= n || w_check < 0 || w_check >= n) continue;
+        else {
+            if (board[h][w] == board[h_check][w_check]) {
+                answer++;
+            }
+        }
+    }
+    return answer;
+}
